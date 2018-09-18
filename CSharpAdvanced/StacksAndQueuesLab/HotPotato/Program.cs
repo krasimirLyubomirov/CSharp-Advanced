cfe5e1b9d75b4010e0ff3117ea80957c03c9e1b5
Krasimir Lyubomirov <40770219+krasimirLyubomirov@users.noreply.github.com>
2018-09-18 16:39:11 +0300

    Add StackAndQueueLab

---
+++ CSharpAdvanced/StacksAndQueuesLab/HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int tossLimit = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(children);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < tossLimit; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
