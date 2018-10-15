84c58d2331d35bfc059ebe7e99ea19f30ca3f76a
Krasimir Lyubomirov <40770219+krasimirLyubomirov@users.noreply.github.com>
2018-10-15 17:52:11 +0300

    Update CreatingConstructors

--- CSharpOOPBasics/DefiningClassesExercise/CreatingConstructors/Person.cs
+++ CSharpOOPBasics/DefiningClassesExercise/CreatingConstructors/Person.cs
@@ -1,35 +1,38 @@
-﻿public class Person
+﻿namespace DefiningClasses
 {
-    private string name;
-    private int age;
-
-    public string Name
+    public class Person
     {
-        get { return name; }
-        set { this.name = value; }
-    }
+        private string name;
+        private int age;
 
-    public int Age
-    {
-        get { return age; }
-        set { this.age = value; }
-    }
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
 
-    public Person()
-    {
-        this.name = "No name";
-        this.age = 1;
-    }
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
 
-    public Person(int age):this()
-    {
-        this.age = age;
-    }
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+        }
 
-    public Person(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
+        public Person(int age) : this()
+        {
+            this.age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
 
