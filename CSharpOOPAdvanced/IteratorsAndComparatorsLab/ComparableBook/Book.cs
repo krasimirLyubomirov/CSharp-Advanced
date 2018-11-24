bd5bc487fbde87abd54bcde31e69ee48efe2ddf1
Krasimir Lyubomirov <40770219+krasimirLyubomirov@users.noreply.github.com>
2018-11-24 20:56:50 +0200

    Add IteratorsAndComparatorsLab

---
+++ CSharpOOPAdvanced/IteratorsAndComparatorsLab/ComparableBook/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.authors = new List<string>(authors);
+    }
+
+    private List<string> authors;
+
+    public string Title { get; private set; }
+
+    public int Year { get; private set; }
+
+    public IReadOnlyCollection<string> Authors => this.authors.AsReadOnly();
+
+    public int CompareTo(Book other)
+    {
+        if (this.Year.CompareTo(other.Year) != 0)
+        {
+            return this.Year.CompareTo(other.Year);
+        }
+
+        return this.Title.CompareTo(other.Title);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
+
